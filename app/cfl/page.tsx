4dad6c6071a0a12ee089b351f87e35a362daf780
Jerrod Mathis <jerrodmathis@proton.me>
2025-01-18 23:46:23 -0500

    removing unused imports

--- app/cfl/page.tsx
+++ app/cfl/page.tsx
@@ -1,55 +1,50 @@
-"use client";
-import React from "react";
-import { ModeToggle } from "../components/mode-toggle";
-import { Avatar, Box, Option, Select, Sheet, Typography } from "@mui/joy";
-import { Header } from "./components/header";
-import { Body } from "./components/body";
+'use client';
+import React from 'react';
+import { Avatar, Box, Option, Select, Sheet, Typography } from '@mui/joy';
 
-import { useUser } from "./api/get-user";
-import { useAvatar } from "./api/avatars";
-import { useLeague } from "./api/leagues";
+import { useAvatar } from './api/avatars';
+import { useLeague } from './api/leagues';
 
-type RecapYear = "2023" | "2024" | "2025";
+type RecapYear = '2023' | '2024' | '2025';
 
 export default function CFL() {
   // const { data: user } = useUser("jmat52");
   // const { data: avatar } = useAvatar(user?.avatar ?? "", "regular");
   // const avatarSrcUrl = avatar ? URL.createObjectURL(avatar) : undefined;
   const { data: league } = useLeague(process.env.SLEEPER_LEAGUE_ID);
-  const { data: leagueAvatar } = useAvatar(league?.avatar ?? "", "regular");
-  const leagueAvatarSrc = leagueAvatar
-    ? URL.createObjectURL(leagueAvatar)
-    : undefined;
+  const { data: leagueAvatar } = useAvatar(league?.avatar ?? '', 'regular');
+  const leagueAvatarSrc =
+    leagueAvatar ? URL.createObjectURL(leagueAvatar) : undefined;
 
-  const [recapYear, setRecapYear] = React.useState<RecapYear>("2025");
+  const [recapYear, setRecapYear] = React.useState<RecapYear>('2025');
 
   return (
     <>
-      <Box display="flex" flexDirection="column">
+      <Box display='flex' flexDirection='column'>
         <Sheet
-          variant="plain"
+          variant='plain'
           sx={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-            flex: "1 1 40",
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            flex: '1 1 40',
             p: 2,
           }}
         >
           <Typography
-            level="h2"
-            component="h1"
+            level='h2'
+            component='h1'
             startDecorator={
-              <Avatar alt="CFL" src={leagueAvatarSrc} sx={{ height: 40 }} />
+              <Avatar alt='CFL' src={leagueAvatarSrc} sx={{ height: 40 }} />
             }
           >
             CFL Yearly Recap
           </Typography>
           <Select defaultValue={recapYear}>
-            <Option value="2025">2025</Option>
-            <Option value="2024">2024</Option>
-            <Option value="2023">2023</Option>
+            <Option value='2025'>2025</Option>
+            <Option value='2024'>2024</Option>
+            <Option value='2023'>2023</Option>
           </Select>
           {/* <Avatar
             alt={user?.display_name ?? ""}
